19f92df3560a361188d103d305228d273e7eb969
masungil <masungil2@daum.net>
2024-10-21 22:37:48 +0900

    RootConfig 클래스 삭제하고 MapperUtil 클래스를 작성하여 사용할 수 있게 소스를 변경함

---
+++ src/main/java/org/kosa/jwt/util/MapperUtil.java
@@ -0,0 +1,26 @@
+package org.kosa.jwt.util;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.config.Configuration.AccessLevel;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MapperUtil {
+	
+	private ModelMapper mapper;
+	
+	MapperUtil() {
+		mapper = new ModelMapper();
+		
+		mapper.getConfiguration()
+			.setFieldAccessLevel(AccessLevel.PRIVATE)
+			.setFieldMatchingEnabled(true)
+			.setMatchingStrategy(MatchingStrategies.STRICT);
+	}
+
+	public <D> D map(Object source, Class<D> destinationType) {
+		return map(source, destinationType); 
+	}
+	
+}

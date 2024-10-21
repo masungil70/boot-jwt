428cab1035f4dc24c8d06589b177e9366103946e
masungil <masungil2@daum.net>
2024-10-21 20:10:52 +0900

    최초 프로젝트 생성 한 후 commit

---
+++ src/test/java/org/kosa/jwt/BootJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kosa.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

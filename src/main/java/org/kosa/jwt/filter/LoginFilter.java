5443506a5a3a1342caabf19a7a8fe0f5cef11aca
masungil <masungil2@daum.net>
2024-10-24 23:07:39 +0900

    로그인 문자열을 json 구성하여 post 방식으로 호출하여 전달된 json 문자열을 LoginFilter에서 얻는 것을 확인한다 실행은 브라우저에서 /login.html를 실행하고 로그인 버트

--- src/main/java/org/kosa/jwt/filter/LoginFilter.java
+++ src/main/java/org/kosa/jwt/filter/LoginFilter.java
@@ -1,10 +1,15 @@
 package org.kosa.jwt.filter;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.util.StreamUtils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,9 +18,13 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class LoginFilter extends AbstractAuthenticationProcessingFilter {
+	private static final String CONTENT_TYPE = "application/json";//json 타입의 데이터로만 로그인을 진행한다.
+	private final ObjectMapper objectMapper;
+
 	
 	public LoginFilter(String defaultFilterPrrocessingUrl) {
 		super(defaultFilterPrrocessingUrl);
+		this.objectMapper = new ObjectMapper();
 	}
 
 	@Override
@@ -24,6 +33,19 @@ public class LoginFilter extends AbstractAuthenticationProcessingFilter {
 		// TODO Auto-generated method stub
 		log.info("LoginFilter ----------------------------- ");
 		
+		log.info("ContentType = {}", request.getContentType());
+		if(request.getContentType() == null || !request.getContentType().startsWith(CONTENT_TYPE)  ) {
+			log.info("Authentication Content-Type not supported: " + request.getContentType());
+			return null;
+		}
+
+		//json으로 요청한 문자열을 얻는다
+		String jsonText = StreamUtils.copyToString(request.getInputStream(), StandardCharsets.UTF_8);
+
+		//JSON 문자열을 Map 객체로 변환 한다
+		Map<String, String> jsonData = objectMapper.readValue(jsonText, Map.class);
+		log.info("jsonData = {}", jsonData);
+
 		return null;
 	}
 	

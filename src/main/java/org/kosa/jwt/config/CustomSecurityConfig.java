f48e1a958fc9ebf171819a368c0338fa238fee81
masungil <masungil2@daum.net>
2024-10-24 22:16:39 +0900

    토크인증을 위해 보안구성시 LoginFilter가 동작 할 수 있게 설정한다 작성 후 브라우저에서 /generateToken URL을 실행하여 LoginFilter인 필터의 실행을 확인한다

--- src/main/java/org/kosa/jwt/config/CustomSecurityConfig.java
+++ src/main/java/org/kosa/jwt/config/CustomSecurityConfig.java
@@ -1,9 +1,13 @@
 package org.kosa.jwt.config;
 
 
+import org.kosa.jwt.MemberDetailsService;
+import org.kosa.jwt.filter.LoginFilter;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,6 +16,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -24,6 +29,9 @@ import lombok.extern.log4j.Log4j2;
 @RequiredArgsConstructor
 public class CustomSecurityConfig {
 
+	
+	private final MemberDetailsService memberDetailsService; 
+	
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -45,6 +53,28 @@ public class CustomSecurityConfig {
 
         log.info("------------configure-------------------");
 
+        //인증관리자 빌더 객체 얻기  
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        
+        //인증관리자에 userDetailsService와 비밀번호 암호화 객체를 설정한다   
+        authenticationManagerBuilder
+        	.userDetailsService(memberDetailsService)
+        	.passwordEncoder(passwordEncoder());
+        
+        //인증관리자를 생성한다 
+        AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+        
+        //http 보안 객체에 인증관리자를 설정한다 
+        http.authenticationManager(authenticationManager);
+        
+        LoginFilter loginFilter = new LoginFilter("/generateToken");
+        loginFilter.setAuthenticationManager(authenticationManager);
+        
+        //UsernamePasswordAuthenticationFilter 필더 객체 실행 전에 동작할 loginFilter를 설정한다 
+        http.addFilterBefore(loginFilter, UsernamePasswordAuthenticationFilter.class);
+        
+        //해당 소스 작성후 : 브라우저에서 /generateToken URL을 실행한다
+        
         //csrf 비활성화 
         http.csrf(csrf -> csrf.disable());
         //세션을 사용하지 않음  
---
+++ src/main/java/org/kosa/jwt/filter/LoginFilter.java
@@ -0,0 +1,31 @@
+package org.kosa.jwt.filter;
+
+import java.io.IOException;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class LoginFilter extends AbstractAuthenticationProcessingFilter {
+	
+	public LoginFilter(String defaultFilterPrrocessingUrl) {
+		super(defaultFilterPrrocessingUrl);
+	}
+
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+			throws AuthenticationException, IOException, ServletException {
+		// TODO Auto-generated method stub
+		log.info("LoginFilter ----------------------------- ");
+		
+		return null;
+	}
+	
+	
+}
